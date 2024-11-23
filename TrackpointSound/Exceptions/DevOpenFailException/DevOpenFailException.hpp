c735fe76c76f4e7c21d145eeb173404429555ee1
Maksim Kononykhin <kononelder@gmail.com>
2024-11-23 04:48:23 +0300

    style-refactoring

--- TrackpointSound/Exceptions/DevOpenFailException/DevOpenFailException.hpp
+++ TrackpointSound/Exceptions/DevOpenFailException/DevOpenFailException.hpp
@@ -3,12 +3,14 @@
 #include <exception>
 #include <string>
 
-class DevOpenFailException : public std::exception {
+class DevOpenFailException : public std::exception
+{
 public:
     explicit DevOpenFailException(const std::string& message)
         : message_(message) {}
 
-    virtual const char* what() const noexcept override {
+    virtual const char* what() const noexcept override
+    {
         return message_.c_str();
     }
 
