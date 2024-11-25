d75a0a3a62c2ef062dd2c367bb6e5fc18545db89
Maksim Kononykhin <kononelder@gmail.com>
2024-11-25 13:56:58 +0300

    add $HOME path

--- TrackpointSound/main.cpp
+++ TrackpointSound/main.cpp
@@ -1,13 +1,14 @@
 #include "MouseDevice.hpp"
 #include "SoundPlayer.hpp"
 #include "DeviceDiscovery.hpp"
+#include <cstdlib>
 
 using utils::MouseDevice;
 using utils::SoundPlayer;
 using utils::DeviceDiscovery;
 
 int main(const int argc, const char* argv[]) {
-    SoundPlayer player("~/.trackpointsound/audio/");
+    SoundPlayer player(std::string(std::getenv("HOME")) + "/.trackpointsound/audio/");
     MouseDevice device(DeviceDiscovery().getTrackpointEventPath());
 
     while (true)
